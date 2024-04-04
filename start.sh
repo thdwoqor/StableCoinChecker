3841241d359da9c15b099491597a17ae83f4bc8d
thdwoqor <celebarte01@gmail.com>
2024-04-04 15:41:31 +0900

    chore: 서브모듈 추가 및 변경된 인프로 적용을 위한 yml 추가

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source ~/.bashrc
+
+if [ -z "$(lsof -i:8081)" ]; then
+    green=8081
+    blue=8080
+else
+    green=8080
+    blue=8081
+fi
+
+cd /home/ec2-user/my-web-server/build/libs/
+nohup java -jar -Dserver.port=$green test-0.0.1-SNAPSHOT.jar &> webserverlog.log &
+
+while [ "$(curl -Isw '%{http_code}' -o /dev/null http://localhost:$green)" == 000 ]
+do
+    echo "새로운 어플리케이션을 실행 중입니다.";
+done
+
+kill -15 $(lsof -ti:$blue)
