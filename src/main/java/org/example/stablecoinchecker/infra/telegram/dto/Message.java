d8633a8e916e736fbb138538bd63fa4ec86ac838
thdwoqor <celebarte01@gmail.com>
2024-04-19 12:38:27 +0900

    feat: 메시지 포멧 수정

--- src/main/java/org/example/stablecoinchecker/infra/telegram/dto/Message.java
+++ src/main/java/org/example/stablecoinchecker/infra/telegram/dto/Message.java
@@ -27,26 +27,26 @@ public class Message {
     ) {
         StringBuilder sb = new StringBuilder();
         sb.append(String.format("• *BTC 기준 USDT 환산*\n"));
-        sb.append("```복사\n");
+//        sb.append("```복사\n");
         sb.append(formatStableCoinInfo(stableCoinInfo));
-        sb.append("```\n");
+//        sb.append("```\n");
         return new Message(sb.toString());
     }
 
     public static Message createStableCoinPricesMessage(final List<StableCoinInfo> stableCoinInfos) {
         StringBuilder sb = new StringBuilder();
         sb.append("• *국내 스테이블 코인 가격*\n");
-        sb.append("```복사\n");
+//        sb.append("```복사\n");
         for (StableCoinInfo coin : stableCoinInfos) {
             sb.append(formatStableCoinInfo(coin));
         }
-        sb.append("```\n");
+//        sb.append("```\n");
         return new Message(sb.toString());
     }
 
     private static String formatStableCoinInfo(final StableCoinInfo stableCoinInfo) {
         return String.format(
-                "%-8s(%s) : %,d원(%.1f%%)\n",
+                "> %-8s(%s) : %,d원(%.1f%%)\n",
                 stableCoinInfo.cex(),
                 stableCoinInfo.symbol(),
                 stableCoinInfo.price(),
