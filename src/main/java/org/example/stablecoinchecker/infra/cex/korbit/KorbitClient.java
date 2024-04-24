11d831015c6e111ba4ff047297479f49b599ef30
thdwoqor <celebarte01@gmail.com>
2024-04-25 02:31:53 +0900

    chore: 코빗 추가

--- src/main/java/org/example/stablecoinchecker/infra/cex/korbit/KorbitClient.java
+++ src/main/java/org/example/stablecoinchecker/infra/cex/korbit/KorbitClient.java
@@ -1,14 +1,17 @@
 package org.example.stablecoinchecker.infra.cex.korbit;
 
+import java.util.Optional;
 import org.example.stablecoinchecker.infra.cex.korbit.dto.KorbitTickerResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "Korbit", url = "https://api.korbit.co.kr")
 public interface KorbitClient {
     @GetMapping("/v1/ticker/detailed?currency_pair={symbol}_{currency}")
-    KorbitTickerResponse getTicker(
+    ResponseEntity<KorbitTickerResponse> getTicker(
             @RequestParam("symbol") String symbol,
             @RequestParam("currency") String currency
     );
--- src/main/java/org/example/stablecoinchecker/infra/cex/korbit/KorbitStableCoinTickerProvider.java
+++ src/main/java/org/example/stablecoinchecker/infra/cex/korbit/KorbitStableCoinTickerProvider.java
@@ -3,12 +3,14 @@ package org.example.stablecoinchecker.infra.cex.korbit;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.example.stablecoinchecker.infra.cex.StableCoinTickerResponse;
 import org.example.stablecoinchecker.infra.cex.StableCoinTickerProvider;
+import org.example.stablecoinchecker.infra.cex.StableCoinTickerResponse;
 import org.example.stablecoinchecker.infra.cex.korbit.dto.KorbitTickerResponse;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+@Service
 @Order(4)
 @RequiredArgsConstructor
 public class KorbitStableCoinTickerProvider implements StableCoinTickerProvider {
@@ -19,8 +21,17 @@ public class KorbitStableCoinTickerProvider implements StableCoinTickerProvider 
     public List<StableCoinTickerResponse> getStableCoinTickers() {
         List<StableCoinTickerResponse> responses = new ArrayList<>();
         for (KorbitStableCoinSymbol value : KorbitStableCoinSymbol.values()) {
-            KorbitTickerResponse response = korbitClient.getTicker(value.getName(), "krw");
-            responses.add(response.toStableCoinTicker(value));
+            ResponseEntity<KorbitTickerResponse> response = korbitClient.getTicker(value.getName(), "krw");
+
+            System.out.println("##");
+            System.out.println(response);
+            System.out.println(response.toString());
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+
+            if (response.getBody() != null) {
+                responses.add(response.getBody().toStableCoinTicker(value));
+            }
         }
         return responses;
     }
