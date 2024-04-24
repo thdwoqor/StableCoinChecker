578055437444cbca49a7ac84b518c5fa9dc1e3e7
thdwoqor <celebarte01@gmail.com>
2024-04-25 00:32:50 +0900

    refactor: 메서드 및 클래스명 변경

--- src/main/java/org/example/stablecoinchecker/service/UpbitConverter.java
+++ src/main/java/org/example/stablecoinchecker/service/UpbitConverter.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UpbitService {
+public class UpbitConverter {
 
     private final UpbitClient upbitClient;
     private final BithumbClient bithumbClient;
--- src/main/java/org/example/stablecoinchecker/service/dto/MessageFormatter.java
+++ src/main/java/org/example/stablecoinchecker/service/dto/MessageFormatter.java
@@ -8,7 +8,7 @@ import org.example.stablecoinchecker.domain.StableCoin;
 @Getter
 public class MessageFormatter {
 
-    public static String createExchangeRateMessage(final BigDecimal exchangeRate) {
+    public static String formatExchangeRateMessage(final BigDecimal exchangeRate) {
         StringBuilder sb = new StringBuilder();
         sb.append("• *환율*\n");
         sb.append("```복사\n");
@@ -17,29 +17,29 @@ public class MessageFormatter {
         return sb.toString();
     }
 
-    public static String createConvertedUsdtPriceMessage(
+    public static String formatConvertedUsdtMessage(
             final StableCoin stableCoinData
     ) {
         StringBuilder sb = new StringBuilder();
         sb.append(String.format("• *BTC 기준 USDT 환산*\n"));
         sb.append("```복사\n");
-        sb.append(formatStableCoinInfo(stableCoinData));
+        sb.append(formatStableCoinPriceMessage(stableCoinData));
         sb.append("```\n");
         return sb.toString();
     }
 
-    public static String createStableCoinPricesMessage(final List<StableCoin> stableCoinData) {
+    public static String formatStableCoinMessage(final List<StableCoin> stableCoinData) {
         StringBuilder sb = new StringBuilder();
         sb.append("• *국내 스테이블 코인 가격*\n");
         sb.append("```복사\n");
         for (StableCoin coin : stableCoinData) {
-            sb.append(formatStableCoinInfo(coin));
+            sb.append(formatStableCoinPriceMessage(coin));
         }
         sb.append("```\n");
         return sb.toString();
     }
 
-    private static String formatStableCoinInfo(final StableCoin stableCoinData) {
+    private static String formatStableCoinPriceMessage(final StableCoin stableCoinData) {
         return String.format(
                 "%-8s(%s) : %,d원(%.1f%%)\n",
                 stableCoinData.getCryptocurrencyExchange().name(),
