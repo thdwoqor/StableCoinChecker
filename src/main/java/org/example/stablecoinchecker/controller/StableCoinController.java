eb0d123aecc1d90b79aaad87c5652c32abd86a0d
thdwoqor <celebarte01@gmail.com>
2024-05-29 16:50:32 +0900

    feat: http method 변경

--- src/main/java/org/example/stablecoinchecker/controller/StableCoinController.java
+++ src/main/java/org/example/stablecoinchecker/controller/StableCoinController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.stablecoinchecker.service.StableCoinService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class StableCoinController {
 
     private final StableCoinService service;
 
-    @PostMapping("/area")
+    @GetMapping("/area")
     public ResponseEntity<List<StableCoinResponse>> findArea(
             @RequestParam("cex") String cex,
             @RequestParam("symbol") String symbol
