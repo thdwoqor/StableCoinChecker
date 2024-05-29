4140df6c7922e829c148c78d08ce774420260c52
thdwoqor <celebarte01@gmail.com>
2024-05-29 17:01:36 +0900

    feat: cors 해결

---
+++ src/main/java/org/example/stablecoinchecker/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package org.example.stablecoinchecker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowCredentials(true);
+        WebMvcConfigurer.super.addCorsMappings(registry);
+    }
+}
