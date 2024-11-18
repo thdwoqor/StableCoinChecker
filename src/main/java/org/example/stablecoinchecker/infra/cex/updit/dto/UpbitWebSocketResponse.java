c4708926564dba91d02a396e43d409b387746d72
thdwoqor <celebarte01@gmail.com>
2024-11-19 03:05:03 +0900

    hotfix: JsonProperty 추가

--- src/main/java/org/example/stablecoinchecker/infra/cex/updit/dto/UpbitWebSocketResponse.java
+++ src/main/java/org/example/stablecoinchecker/infra/cex/updit/dto/UpbitWebSocketResponse.java
@@ -1,6 +1,7 @@
 package org.example.stablecoinchecker.infra.cex.updit.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties
 public class UpbitWebSocketResponse {
 
+    @JsonProperty("trade_price")
     private BigDecimal tradePrice;
     private Long timestamp;
     private String code;
