35955cc3ea2c6946600837b73c66d0cd3dbc7f08
thdwoqor <celebarte01@gmail.com>
2024-11-19 02:38:09 +0900

    fix: Json 데이터 변환시 알수없는 property를 무시하는 문제 해결

--- src/test/java/org/example/stablecoinchecker/infra/cex/JsonUtilsTest.java
+++ src/test/java/org/example/stablecoinchecker/infra/cex/JsonUtilsTest.java
@@ -5,18 +5,18 @@ import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.example.stablecoinchecker.infra.cex.updit.dto.UpbitWebSocketResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class JsonUtilsTest {
 
-    //https://junuuu.tistory.com/821
     @Test
     void 타입이_맞지않는경우_empty_를_반환한다() {
+        final JsonUtils jsonUtils = new JsonUtils(new ObjectMapper());
         String message = "{\"status\":\"UP\"}";
-
-        JsonUtils jsonUtils = new JsonUtils(new ObjectMapper());
-
         Optional<UpbitWebSocketResponse> deserialize = jsonUtils.deserialize(message, UpbitWebSocketResponse.class);
 
         Assertions.assertThat(deserialize).isNotPresent();
     }
+
 }
